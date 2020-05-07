337fde84ab8d643f523c1f8f0da450f7eacdb944
robertalanis <rxalanis@gmail.com>
2020-05-07 00:31:25 -0500

    initial files

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO burger (name) VALUES ('Big Mac');
+INSERT INTO burger (name) VALUES ('McDouble');
+INSERT INTO burger (name) VALUES ('Whopper');
+INSERT INTO burger (name) VALUES ('Quarter Pounder');
+
---
+++ server.js
@@ -0,0 +1,6 @@
+// ==============================================================================
+// DEPENDENCIES
+// Series of npm packages that we will use to give our server useful functionality
+// ==============================================================================
+
+var express = require("express");
